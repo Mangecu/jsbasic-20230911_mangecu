2215198ecdd1715b02837b6c98d5a9691e3c9f46
Ragimov Fuad <foyado@gmail.com>
2023-10-09 00:16:24 +0300

    started 6-module 1-task

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -13,6 +13,57 @@
  *
  */
 export default class UserTable {
+
+  elem = null;
+
+  #rows = [];
+
+
   constructor(rows) {
+    this.#rows = rows || this.#rows;
+    this.#render();
   }
+  #render() {
+    this.elem = this.createDOMElement(this.#template());
+
+  }
+
+  #templateRows(item) {
+    let list = document.createElement('tr');
+    for (let index in item) {
+      let row = document.createElement('td');
+      row.innerText = `${item[index]}`
+      list.append(row);
+    }
+    return list
+  }
+
+  createDOMElement (html) {
+    const temp = document.createElement('table');
+    temp.innerHTML = html;
+    return temp.firstElementChild;
+  }
+
+  #template() {
+    return `
+      <table>
+         <thead>
+            <tr>
+              <th>Имя</th>
+              <th>Возраст</th>
+              <th>Зарплата</th>
+              <th>Город</th>
+              <th></th>
+            </tr>
+         </thead>
+         <tbody>
+            ${this.#rows.forEach(item => this.#templateRows(item))};
+         </tbody>
+      </table>
+    `
+  }
+
+
 }
+
+
