6f64f7e33315a2bf00b0f6c301400836c73984e8
Ragimov Fuad <foyado@gmail.com>
2023-10-23 00:06:12 +0300

    task-3 needs to be completed

--- 6-module/2-task/index.js
+++ 6-module/2-task/index.js
@@ -1,4 +1,50 @@
 export default class ProductCard {
+
+  elem = null;
+
   constructor(product) {
+    this.name = product.name;
+    this.price = product.price;
+    this.category = product.category;
+    this.image = product.image;
+    this.id = product.id;
+
+    this.#render()
+  }
+
+  #render() {
+    this.elem = this.createDOMElement(this.#template(), 'div');
+  }
+
+  createDOMElement (html, tag) {
+    const temp = document.createElement(tag);
+    temp.innerHTML = html;
+    return temp.firstElementChild;
+
+  }
+
+  #template() {
+    return `
+      <div class="card">
+         <div class="card__top">
+            <img src="/assets/images/products/${this.image}" class="card__image" alt="product">
+            <span class="card__price">€${this.price.toFixed(2)}</span>
+         </div>
+         <div class="card__body">
+            <div class="card__title">${this.name}</div>
+            <button type="button" class="card__button">
+               <img src="/assets/images/icons/plus-icon.svg" alt="icon">
+            </button>
+         </div>
+      </div>
+    `
+  }
+
+  #addToCart = () => {
+    const event = new CustomEvent('product-add', {
+      detail: this.id,
+      bubbles: true
+    })
+    this.elem.dispatchEvent(event);
   }
 }
--- 6-module/3-task/index.js
+++ 6-module/3-task/index.js
@@ -1,7 +1,54 @@
 import createElement from '../../assets/lib/create-element.js';
 
 export default class Carousel {
+
+  elem = null;
+
   constructor(slides) {
+    this.count = 0;
     this.slides = slides;
+    this.#render();
+
   }
+
+  #render() {
+    this.elem = this.createDOMElement(this.#template(), 'div');
+
+  }
+
+  createDOMElement(html, tag) {
+    const temp = document.createElement(tag);
+    temp.innerHTML = html;
+    return temp.firstElementChild;
+  }
+
+  #template() {
+    return `
+      <div class="carousel">
+         <div class="carousel__arrow carousel__arrow_right">
+            <img src="/assets/images/icons/angle-icon.svg" alt="icon">
+         </div>
+         <div class="carousel__arrow carousel__arrow_left">
+            <img src="/assets/images/icons/angle-left-icon.svg" alt="icon">
+         </div>
+         <div class="carousel__inner">
+            ${this.slides
+              .map(item => `<div class="carousel__slide" data-id="${item.id}">
+                              <img src="/assets/images/carousel/${item.image}" class="carousel__img" alt="slide">
+                              <div class="carousel__caption">
+                                 <span class="carousel__price">€${item.price.toFixed(2)}</span>
+                                 <div class="carousel__title">${item.name}</div>
+                                 <button type="button" class="carousel__button">
+                                    <img src="/assets/images/icons/plus-icon.svg" alt="icon">
+                                 </button>
+                              </div>
+                            </div>`)
+              .join('\n')}
+         </div>
+      </div>
+    `
+  }
+
+
+
 }
