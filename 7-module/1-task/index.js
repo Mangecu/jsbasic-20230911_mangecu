fafd18b848c3637796cf05dd435af42a2e3f2716
Ragimov Fuad <foyado@gmail.com>
2023-10-29 22:56:24 +0300

    finished module-7 task-1, started task-2

--- 7-module/1-task/index.js
+++ 7-module/1-task/index.js
@@ -11,7 +11,7 @@ export default class RibbonMenu {
 
   #render() {
     this.elem = this.createDOMElement(this.#template(), 'div');
-    this.#scroll(this.elem);
+    this.#initRibbon(this.elem);
   }
 
   createDOMElement(html, tag) {
@@ -23,7 +23,7 @@ export default class RibbonMenu {
   #template() {
     return `
       <div class="ribbon">
-         <button class="ribbon__arrow ribbon__arrow_left ribbon__arrow_visible">
+         <button class="ribbon__arrow ribbon__arrow_left">
             <img src="/assets/images/icons/angle-icon.svg" alt="icon">
          </button>
          <nav class="ribbon__inner">
@@ -31,19 +31,63 @@ export default class RibbonMenu {
               .map(item => `<a href="#" class="ribbon__item" data-id="${item.id}">${item.name}</a>`)
               .join('\n')}
          </nav>
-         <button class="ribbon__arrow ribbon__arrow_right">
+         <button class="ribbon__arrow ribbon__arrow_right ribbon__arrow_visible">
             <img src="/assets/images/icons/angle-icon.svg" alt="icon">
          </button>
       </div>
     `
   }
 
-  #scroll(elem) {
+  #initRibbon(elem) {
     let ribbon = elem.querySelector('.ribbon__inner');
     let ribbonArrow = elem.querySelectorAll('.ribbon__arrow');
+    let ribbonItems = ribbon.querySelectorAll('.ribbon__item');
 
-    // ribbonArrow.forEach(button => {
-    //   button.addEventListener('click', )
-    // })
+    ribbonArrow.forEach(button => {
+      button.addEventListener('click', event => {
+        if (event.currentTarget.classList.contains('ribbon__arrow_left')) {
+          ribbon.scrollBy(-350, 0);
+        } else if (event.currentTarget.classList.contains('ribbon__arrow_right')) {
+          ribbon.scrollBy(350, 0);
+        }
+      })
+
+      ribbon.addEventListener('scroll', () => {
+        this.hiddenArrow(ribbon, ribbonArrow);
+      })
+    })
+
+    ribbonItems.forEach(item => {
+      item.addEventListener('click', event => {
+        event.preventDefault();
+        this.cleanLinks(ribbonItems);
+        event.currentTarget.classList.add('ribbon__item_active');
+        item.dispatchEvent(new CustomEvent('ribbon-select', {
+          detail: item.dataset.id,
+          bubbles: true
+        }))
+      })
+    })
   }
+
+  hiddenArrow (ribbon, ribbonArrow) {
+    let scrollLeft = ribbon.scrollLeft;
+    let scrollRight = ribbon.scrollWidth - scrollLeft - ribbon.clientWidth;
+
+    if (scrollLeft === 0) {
+      ribbonArrow[0].classList.remove('ribbon__arrow_visible');
+    } else if (scrollRight < 1) {
+      ribbonArrow[1].classList.remove('ribbon__arrow_visible');
+    } else {
+      ribbonArrow[0].classList.add('ribbon__arrow_visible');
+      ribbonArrow[1].classList.add('ribbon__arrow_visible');
+    }
+  }
+
+  cleanLinks(arr) {
+    for (let i = 0; i < arr.length; i++) {
+      arr[i].classList.remove('ribbon__item_active');
+    }
+  }
+
 }
--- 7-module/2-task/index.js
+++ 7-module/2-task/index.js
@@ -1,6 +1,56 @@
 import createElement from '../../assets/lib/create-element.js';
 
 export default class Modal {
+
+  elem = null;
+  title;
+  html;
+
   constructor() {
+
   }
+
+  open() {
+    this.elem = this.createDOMElement(this.#template(), 'div');
+    document.querySelector('.container').append(this.elem);
+    console.log(this);
+  }
+
+  createDOMElement(html, tag) {
+    const temp = document.createElement(tag);
+    temp.innerHTML = html;
+    return temp.firstElementChild;
+  }
+
+  setTitle(str) {
+      return this.elem ? this.elem.querySelector('.modal__title').innerHTML = str : this.title = str;
+  }
+
+  setBody(html) {
+      // console.log(html)
+    this.elem.querySelector('.modal__body').innerHTML = '';
+    return this.elem ? this.elem.querySelector('.modal__body').innerHTML = html : this.html = html;
+  }
+
+  #template() {
+    return `
+      <div class="modal">
+         <div class="modal__overlay"></div>
+         <div class="modal__inner">
+            <div class="modal__header">
+               <button type="button" class="modal__close">
+                  <img src="/assets/images/icons/cross-icon.svg" alt="close-icon" />
+               </button>
+               <h3 class="modal__title">
+                  ${this.title}
+               </h3>
+            </div>
+            <div class="modal__body">
+               ${this.html}
+            </div>
+         </div>
+      </div>
+    `
+  }
+
 }
